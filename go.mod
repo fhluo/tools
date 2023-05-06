5db2acddb2ff1015f82ad3215782383d157c5fd3
fhluo <20843637+fhluo@users.noreply.github.com>
2023-05-06 18:26:49 +0800

    feat: add hibernate

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/samber/lo v1.38.1
 	github.com/spf13/cobra v1.7.0
 	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
+	golang.org/x/sys v0.8.0
 	golang.org/x/text v0.9.0
 )
 
@@ -13,6 +14,5 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/tools v0.8.0 // indirect
 )
---
+++ hibernate/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slog"
+	"golang.org/x/sys/windows"
+	"log"
+	"os"
+)
+
+var ProcSetSuspendState = windows.NewLazyDLL("PowrProf.dll").NewProc("SetSuspendState")
+
+func SetSuspendState(hibernate bool, disableWakeupEvents bool) error {
+	var (
+		hibernate_           uintptr
+		force_               uintptr
+		disableWakeupEvents_ uintptr
+	)
+
+	if hibernate {
+		hibernate_ = 1
+	}
+	if disableWakeupEvents {
+		disableWakeupEvents_ = 1
+	}
+
+	r, _, err := ProcSetSuspendState.Call(
+		hibernate_,
+		force_,
+		disableWakeupEvents_,
+	)
+	if r == 0 {
+		return err
+	}
+
+	return nil
+}
+
+var rootCmd = cobra.Command{
+	Use:  "hibernate",
+	Args: cobra.ExactArgs(0),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return SetSuspendState(hibernate, disableWakeupEvents)
+	},
+}
+
+var (
+	hibernate           bool
+	disableWakeupEvents bool
+)
+
+func init() {
+	log.SetFlags(0)
+
+	rootCmd.PersistentFlags().BoolP("help", "", false, "help for hibernate")
+	rootCmd.Flags().BoolVarP(&hibernate, "hibernate", "h", true, "hibernate or suspend")
+	rootCmd.Flags().BoolVarP(&disableWakeupEvents, "disableWakeupEvents", "d", false, "disables all wake events")
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		slog.Error("failed to execute root command", "err", err)
+		os.Exit(1)
+	}
+}
